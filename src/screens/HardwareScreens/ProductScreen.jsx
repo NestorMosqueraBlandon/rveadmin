bb898b7d1edc06b9c7cf82ac53f3712bb0584f9a
Space Polux <spacepoluxmanager@gmail.com>
2021-09-14 20:02:26 -0500

    reset commit

--- src/screens/HardwareScreens/ProductScreen.jsx
+++ src/screens/HardwareScreens/ProductScreen.jsx
@@ -23,9 +23,7 @@ export default function ProductScreen(props) {
   const { loading, error, products } = productList;
 
   const productCreate = useSelector((state) => state.productCreate);
-  const {
-    success: successCreate,
-  } = productCreate;
+  const { success: successCreate } = productCreate;
 
   const productDelete = useSelector((state) => state.productDelete);
   const {
@@ -118,16 +116,17 @@ export default function ProductScreen(props) {
   };
   return (
     <>
+      {loadingCategories && <LoadingBox></LoadingBox>}
       {errorCategories && (
         <MessageBox variant="danger">{errorCategories}</MessageBox>
       )}
-      {loadingCategories ? (
-        <LoadingBox></LoadingBox>
-      ) : (
-        <div className="card">
-          <div className="card__header">
-            <h2>Products</h2>
-            <div className="buttons">
+      <div className="card">
+        <div className="card__header">
+          <h2>Products</h2>
+          <div className="buttons">
+            {loadingCategories ? (
+              <LoadingBox></LoadingBox>
+            ) : (
               <select
                 name=""
                 id=""
@@ -135,115 +134,182 @@ export default function ProductScreen(props) {
                 onChange={(e) => setSearchCategory(e.target.value)}
               >
                 <option value="">ALL</option>
+
                 {categories.map((categorie) => (
                   <option key={categorie._id} value={categorie._id}>
                     {categorie.name.toUpperCase()}
                   </option>
                 ))}
               </select>
-              <select
-                name=""
-                id=""
-                value={searchBrand}
-                onChange={(e) => setSearchBrand(e.target.value)}
-              >
-                <option value="">ALL</option>
-                <option value="AMD">AMD</option>
-                <option value="INTEL">INTEL</option>
-              </select>
-              <input
-                type="text"
-                placeholder="Search a product"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <button className="btn" onClick={() => setOpenModal(!openModal)}>
-                Create
-              </button>
-            </div>
+            )}
+            <select
+              name=""
+              id=""
+              value={searchBrand}
+              onChange={(e) => setSearchBrand(e.target.value)}
+            >
+              <option value="">ALL</option>
+              <option value="AMD">AMD</option>
+              <option value="INTEL">INTEL</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Search a product"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button className="btn" onClick={() => setOpenModal(!openModal)}>
+              Create
+            </button>
           </div>
         </div>
-      )}
+      </div>
 
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox>{error}</MessageBox>
-      ) : (
-        <>
-          {
-            <table>
-              <thead>
-                <tr className="thead">
-                  <th>Image</th>
-                  <th>Name</th>
-                  <th>Category</th>
-                  <th>Brand</th>
-                  <th>Price</th>
-                  <th>Create At</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
+      <>
+        {
+          <table>
+            <thead>
+              <tr className="thead">
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Brand</th>
+                <th>Price</th>
+                <th>Create At</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            {/* {error && <MessageBox>{error}</MessageBox>} */}
+            {loading ? (
+              <LoadingBox></LoadingBox>
+            ) : (
               <tbody>
-               
+                {products
+                  .filter((product) =>
+                    product.brand
+                      .toLowerCase()
+                      .includes(searchBrand.toLowerCase())
+                  )
 
-              {errorCategories && <MessageBox variant="danger">{errorCategories}</MessageBox>}
-                {loadingCategories && <LoadingBox></LoadingBox>}
-                    {products
-                      .filter((product) =>
-                        product.brand
-                          .toLowerCase()
-                          .includes(searchBrand.toLowerCase())
-                      )
-                      .filter((product) =>
-                        product.category
-                          .toLowerCase()
-                          .includes(searchCategory.toLowerCase())
-                      )
-                      .filter((product) =>
-                        product.name
-                          .toLowerCase()
-                          .includes(search.toLowerCase())
-                      )
-                      .map((product) => (
-                        <tr key={product._id}>
-                          <td className="product__image">
-                            <img src={product.image} alt="" />
-                          </td>
-                          <td>{product.name}</td>
-                          {errorCategories && <MessageBox variant="danger">{errorCategories}</MessageBox>}
-                {loadingCategories && <LoadingBox></LoadingBox>}
+                  .filter((product) =>
+                    product.category
+                      .toLowerCase()
+                      .includes(searchCategory.toLowerCase())
+                  )
+                  .filter((product) =>
+                    product.name.toLowerCase().includes(search.toLowerCase())
+                  )
+                  .map((product) => (
+                    <tr key={product._id}>
+                      <td className="product__image">
+                        <img src={product.image} alt="" />
+                      </td>
+                      <td>{product.name}</td>
+                      {loadingCategories ? (
+                        <LoadingBox></LoadingBox>
+                      ) : (
+                        <>
                           {categories
                             .filter(
                               (categorie) => categorie._id === product.category
                             )
                             .map((categorie) => (
-                              <td>{categorie.name}</td>
+                              <td>{categorie.name.toUpperCase()}</td>
                             ))}
-                          <td>{product.brand}</td>
-                          <td>
-                            <DivisaFormater value={product.price} />
-                          </td>
-                          <td>{timeago.format(product.createdAt, 'en_US')}</td>
-                          <td>
-                            <button className="btn-icon">
-                              <i className="bx bx-pencil"></i>
-                            </button>
-                            <button
-                              className="btn-icon"
-                              onClick={() => deleteHandler(product)}
-                            >
-                              <i className="bx bx-trash-alt"></i>
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                 
+                        </>
+                      )}
+                      <td>{product.brand}</td>
+                      <td>
+                        <DivisaFormater value={product.price} />
+                      </td>
+                      <td>{timeago.format(product.createdAt, 'en_US')}</td>
+                      <td>
+                        <button className="btn-icon">
+                          <i className="bx bx-pencil"></i>
+                        </button>
+                        <button
+                          className="btn-icon"
+                          onClick={() => deleteHandler(product)}
+                        >
+                          <i className="bx bx-trash-alt"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
-            </table>
-          }
-        </>
-      )}
+            )}
+          </table>
+        }
+      </>
+
+      <div className={openModal ? 'modal active' : 'modal'}>
+        <div className="modal__dialog">
+          <div className="modal__card">
+            <div className="card__header b-line">
+              <h2 className="card__title">Add Product</h2>
+              <button
+                className="card__title btn-icon"
+                onClick={() => setOpenModal(!openModal)}
+              >
+                <i className="bx bxs-x-circle"></i>
+              </button>
+            </div>
+            <div className="card__body">
+              <form action="">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Brand"
+                    value={brand}
+                    onChange={(e) => setBrand(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  {loadingCategories ? (
+                    <LoadingBox></LoadingBox>
+                  ) : (
+                    <select
+                      name=""
+                      id=""
+                      value={category}
+                      onChange={(e) => setCategory(e.target.value.toUpperCase())}
+                    >
+                      <option value="">ALL</option>
+
+                      {categories.map((categorie) => (
+                        <option key={categorie._id} value={categorie._id}>
+                          {categorie.name.toUpperCase()}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="card__footer">
+              <button className="btn" onClick={submitHandler}>
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
