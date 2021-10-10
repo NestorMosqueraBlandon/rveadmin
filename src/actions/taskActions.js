61a53e8be166275ccdca1765305758195703f839
Nestor Mosquera <nestor@nestor-14IWL.(none)>
2021-10-10 00:17:07 -0500

    update task two

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -31,7 +31,7 @@ export const createTask = (priority, title, description, users) => async(dispatc
     dispatch({type: TASK_CREATE_REQUEST, payload: {priority, title, description, users}});
     
     try{
-        const {data} = await Axios.post('http://rveapi.herokuapp.com/api/v1/tasks/', {priority, title, description, users});
+        const {data} = await Axios.post('https://rveapi.herokuapp.com/api/v1/tasks/', {priority, title, description, users});
         dispatch({type: TASK_CREATE_SUCCESS, payload: data});
     }catch(error){
         const message = error.response && error.response.data.message ? error.response.data.message : error.message;
@@ -43,7 +43,7 @@ export const updateTask = (task) => async(dispatch) => {
     dispatch({type: TASK_UPDATE_REQUEST, payload: task});
     
     try{
-        const {data} = await Axios.put(`http://rveapi.herokuapp.com/api/v1/tasks/${task._id}`, task);
+        const {data} = await Axios.put(`https://rveapi.herokuapp.com/api/v1/tasks/${task._id}`, task);
         dispatch({type: TASK_UPDATE_SUCCESS, payload: data});
     }catch(error){
         const message = error.response && error.response.data.message ? error.response.data.message : error.message;
@@ -56,7 +56,7 @@ export const updateTask = (task) => async(dispatch) => {
 export const deleteCategory = (id) => async(dispatch, getState) => {
     dispatch({type: TASK_DELETE_REQUEST, payload: id});
     try{
-        Axios.delete(`http://rveapi.herokuapp.com/api/v1/categories/${id}`);
+        Axios.delete(`https://rveapi.herokuapp.com/api/v1/categories/${id}`);
         dispatch({type: TASK_DELETE_SUCCESS})
     }catch(error){
         dispatch({type: TASK_DELETE_FAIL, payload: error.message && error.response.data.message? error.response.data.message : error.message, })
