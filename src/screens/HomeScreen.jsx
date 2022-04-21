b10686e646600e8d88705342970d8ad4c80c4eb4
Nestor Mosquera <yunsde18@gmail.com>
2022-04-21 16:58:24 -0500

    new update

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { BarChart } from 'recharts';
 
 export default function HomeScreen() {
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
+
+  const data = []
+
 
   return (
     <div className="clean__card">
@@ -12,6 +16,14 @@ export default function HomeScreen() {
           Hi {userInfo.name} <br />{' '}
           <span className="home__subtitle"> Welcome Back </span>
         </h2>
+      </div>
+
+      <div className="list__card">
+        <div className="card">
+          <BarChart width={400} height={400} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}> 
+        
+          </BarChart>
+        </div>
       </div>
     </div>
   );
